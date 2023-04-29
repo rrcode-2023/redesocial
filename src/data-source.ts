b2f576250fc9423050ee0391b6a283e3b1e046ae
richzago <rich.czago@gmail.com>
2023-04-29 16:01:45 -0300

    Initial Commit

---
+++ src/data-source.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import "dotenv/config";
+export const AppDataSource = new DataSource(
+  process.env.NODE_ENV === "test"
+    ? {
+        type: "sqlite",
+        database: ":memory:",
+        synchronize: true,
+        entities: ["src/entities/*.ts"],
+      }
+    : {
+        type: "mongodb",
+      }
+);
