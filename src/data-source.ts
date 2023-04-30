e259ae13cec7f3b234f36d7c5dfc5b425fc973a1
richzago <rich.czago@gmail.com>
2023-04-30 03:17:37 -0300

    feat: configured database

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,6 @@
 import { DataSource } from "typeorm";
 import "dotenv/config";
+
 export const AppDataSource = new DataSource(
   process.env.NODE_ENV === "test"
     ? {
@@ -10,5 +11,12 @@ export const AppDataSource = new DataSource(
       }
     : {
         type: "mongodb",
+        url: `${process.env.MONGODB_URL}`,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        logging: true,
+        synchronize: true,
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"],
       }
 );
