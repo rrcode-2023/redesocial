2e430ec73c7f8df7d861f4a46772f0f766e37588
richzago <rich.czago@gmail.com>
2023-04-30 17:04:13 -0300

    feat: created post model

---
+++ src/models/post.models.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { IUser } from "./user.model";
+
+export interface IPost extends mongoose.Document {
+  content: String;
+  imgPost: String;
+  createdAt: Date;
+  updatedAt?: Date;
+  author: IUser["_id"];
+}
+
+const postSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  imgPost: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    required: true,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+const PostModel = mongoose.model("Post", postSchema);
+
+export { PostModel };
