3a24c8c3bcd6a31fbaf1e5a08a4a93973ddf1070
richzago <rich.czago@gmail.com>
2023-04-30 17:03:53 -0300

    feat: typed function return

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -1,11 +1,7 @@
-import { UserModel } from "../../models/user.model";
-const createUserService = async (userData: any) => {
-  try {
-    const user = new UserModel(userData);
-    return await user.save();
-  } catch (error) {
-    console.error(error);
-  }
+import { IUser, UserModel } from "../../models/user.model";
+const createUserService = async (userData: any): Promise<IUser> => {
+  const user = new UserModel(userData);
+  return await user.save();
 };
 
 export { createUserService };
