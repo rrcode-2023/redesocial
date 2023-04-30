4b7f0ad167e8f1308c588b12f69b4fc39a4a5dde
richzago <rich.czago@gmail.com>
2023-04-30 13:48:31 -0300

    feat: route being called

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import "reflect-metadata";
 import "express-async-errors";
 import express, { Application } from "express";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
+import { userRouter } from "./routes/user.routes";
 
 const app: Application = express();
 app.use(express.json());
+
+app.use("/users", userRouter);
 
 app.use(handleErrorMiddleware);
 
