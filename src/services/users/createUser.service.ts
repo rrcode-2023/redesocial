3e339b4761f650193b293fe7f4e60710328aaf5e
richzago <rich.czago@gmail.com>
2023-05-01 14:13:54 -0300

    feat: added hash to password

--- src/services/users/createUser.service.ts
+++ src/services/users/createUser.service.ts
@@ -1,6 +1,15 @@
 import { IUser, UserModel } from "../../models/user.model";
-const createUserService = async (userData: any): Promise<IUser> => {
-  const user = new UserModel(userData);
+import bcryptjs from "bcryptjs";
+const createUserService = async (userData: IUser): Promise<IUser> => {
+  const { password, ...rest } = userData;
+  const salt = await bcryptjs.genSalt(10);
+  const hashedPassword = await bcryptjs.hash(password, salt);
+
+  const user = new UserModel({
+    ...rest,
+    password: hashedPassword,
+  });
+
   return await user.save();
 };
 
