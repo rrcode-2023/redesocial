d7e322c6b6d96e88abea139e07b7c3943692e75c
richzago <rich.czago@gmail.com>
2023-05-01 14:13:26 -0300

    feat: Created new routes and added middlewares

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,7 +1,41 @@
 import { Router } from "express";
-import { createUserController, deleteUserController } from "../controllers/user.controller";
+import {
+  createUserController,
+  deleteUserController,
+  listUserController,
+  updateUserController,
+} from "../controllers/user/user.controller";
+import { validateUserBody } from "../middlewares/validateBody.middleware";
+import checkDuplicateEmailMiddleware from "../middlewares/checkDuplicateEmail.middleware";
+import { validateIdMiddleware } from "../middlewares/validateId.middleware";
+import { AuthUserMiddleware } from "../middlewares/authUser.middleware";
+import { authorizeUserAction } from "../middlewares/authorizeUserAction";
 
 export const userRouter = Router();
 
-userRouter.post("", createUserController);
-userRouter.delete("/:id", deleteUserController);
+userRouter.post(
+  "",
+  validateUserBody,
+  checkDuplicateEmailMiddleware,
+  createUserController
+);
+userRouter.get(
+  "/:id",
+  AuthUserMiddleware,
+  authorizeUserAction,
+  listUserController
+);
+userRouter.delete(
+  "/:id",
+  validateIdMiddleware,
+  AuthUserMiddleware,
+  authorizeUserAction,
+  deleteUserController
+);
+userRouter.patch(
+  "/:id",
+  validateIdMiddleware,
+  AuthUserMiddleware,
+  authorizeUserAction,
+  updateUserController
+);
