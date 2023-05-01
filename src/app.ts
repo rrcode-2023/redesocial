d6f02da97c6eef600e4b6735ef3d8a9815254380
richzago <rich.czago@gmail.com>
2023-05-01 14:10:50 -0300

    feat: added route login

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import "express-async-errors";
 import express, { Application } from "express";
 import handleErrorMiddleware from "./middlewares/handleError.middleware";
 import { userRouter } from "./routes/user.routes";
+import { sessionRouter } from "./routes/session.routes";
 
 const app: Application = express();
 app.use(express.json());
 
 app.use("/users", userRouter);
+app.use("/login", sessionRouter);
 
 app.use(handleErrorMiddleware);
 
