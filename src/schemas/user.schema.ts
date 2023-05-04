b21e45d68da3d7621d2b94d07e3e72f3d0204a07
lgrimm6 <robertofernandesfreitas@gmail.com>
2023-05-04 12:51:09 -0300

    Fix: birthdate field validation

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,4 +1,5 @@
 import * as z from "zod";
+import { AppError } from "../errors/appError";
 
 export const userSchema = z.object({
   firstName: z.string().min(3).max(255),
@@ -6,11 +7,14 @@ export const userSchema = z.object({
   email: z.string().email().max(255),
   birthDate: z.string().refine((val) => {
     const regex = /^\d{4}-\d{2}-\d{2}$/;
-    if (!regex.test(val))
-      throw new Error("Expected date with format 'YYYY-MM-DD'");
-    const date = new Date(val);
-    return !isNaN(date.getTime());
-  }),
+    if (!regex.test(val)){
+      return false
+    }
+    const date = new Date(val)
+    return !isNaN(date.getTime())
+    }, {
+      message: "Invalid date format, expected 'YYYY-MM-DD'",
+    }),
   password: z.string().min(5),
   gender: z.string().min(2).max(15),
   profilePicture: z.string().max(255).nullable().default(null),
